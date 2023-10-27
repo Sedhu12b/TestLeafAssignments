b8977a57e6209456d38509034c35f01ccfb4c122
Sedhu12b <147420049+Sedhu12b@users.noreply.github.com>
2023-10-27 10:03:37 +0530

    Add files via upload
    OctWekdaybatchWeek1Day1Commit

---
+++ day1/FirstCodeSeTestLeaf.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class FirstCodeSeTestLeaf {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Welcom to TestLeaf");
+	}
+
+}
